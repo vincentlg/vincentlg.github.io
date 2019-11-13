41e3fc53ef46c3384558d84820d84c3f8c2737f5
Bettina Boon Falleur <bettina.boonfalleur@gmail.com>
2019-11-13 12:32:36 +0100

    building speakers js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function($) {
 
 	"use strict";
 
-	
+
 
 	var siteMenuClone = function() {
 
@@ -19,11 +19,11 @@ jQuery(document).ready(function($) {
 
 
 		setTimeout(function() {
-			
+
 			var counter = 0;
       $('.site-mobile-menu .has-children').each(function(){
         var $this = $(this);
-        
+
         $this.prepend('<span class="arrow-collapse collapsed">');
 
         $this.find('.arrow-collapse').attr({
@@ -49,8 +49,8 @@ jQuery(document).ready(function($) {
       } else {
         $this.addClass('active');
       }
-      e.preventDefault();  
-      
+      e.preventDefault();
+
     });
 
 		$(window).resize(function() {
@@ -75,7 +75,7 @@ jQuery(document).ready(function($) {
 				$('body').addClass('offcanvas-menu');
 				$this.addClass('active');
 			}
-		}) 
+		})
 
 		// click outisde offcanvas
 		$(document).mouseup(function(e) {
@@ -86,7 +86,7 @@ jQuery(document).ready(function($) {
 				}
 	    }
 		});
-	}; 
+	};
 	siteMenuClone();
 
 
@@ -218,7 +218,7 @@ jQuery(document).ready(function($) {
 
 	var siteCountDown = function() {
 
-		$('#date-countdown').countdown('2020/10/10', function(event) {
+		$('#date-countdown').countdown('2020/03/03 GMT+1 09:30', function(event) {
 		  var $this = $(this).html(event.strftime(''
 		    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
 		    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
@@ -226,7 +226,7 @@ jQuery(document).ready(function($) {
 		    + '<span class="countdown-block"><span class="label">%M</span> min </span>'
 		    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
 		});
-				
+
 	};
 	siteCountDown();
 
---
+++ speakers-json.js
@@ -0,0 +1,69 @@
+
+var speakers = [
+  {
+    "name": "Vlad Zamfir",
+    "link": "https://twitter.com/VladZamfir",
+    "picture": "images/speakers/3.jpg",
+    "company": "Ethereum Foundation"
+  },
+  {
+    "name": "Taylor Monahan",
+    "link": "https://twitter.com/tayvano_",
+    "picture": "images/speakers/145.jpg",
+    "company": "MyCrypto"
+  },
+  {
+    "name": "Fabian Vogelsteller",
+    "link": "https://twitter.com/feindura",
+    "picture": "images/speakers/28.jpg",
+    "company": "LUKSO"
+  },
+  {
+    "name": "Hudson Jameson",
+    "link": "https://twitter.com/hudsonjameson",
+    "picture": "images/speakers/23.jpg",
+    "company": "Ethereum Foundation"
+  },
+  {
+    "name": "Aya Miyaguchi",
+    "link": "https://twitter.com/mi_ayako",
+    "picture": "images/speakers/199.jpg",
+    "company": "Ethereum Foundation"
+  },
+  {
+    "name": "Gilles Fedak",
+    "link": "https://www.linkedin.com/in/gilles-fedak-4a303a5b/",
+    "picture": "images/speakers/89.jpg",
+    "company": "iExec"
+  },
+  {
+    "name": "Robert Habermeier",
+    "link": "https://twitter.com/rphmeier",
+    "picture": "images/speakers/27.jpg",
+    "company": "Polkadot"
+  },
+  {
+    "name": "Lane Rettig",
+    "link": "https://twitter.com/lrettig",
+    "picture": "images/speakers/18.jpg",
+    "company": "Ethereum Foundation"
+  },
+  {
+    "name": "Christoph Jentzsch",
+    "link": "https://twitter.com/ChrJentzsch",
+    "picture": "images/speakers/1.jpg",
+    "company": "Slock.it"
+  },
+  {
+    "name": "Martin Lundfall",
+    "link": "https://twitter.com/MartinLundfall",
+    "picture": "images/speakers/2.jpg",
+    "company": "Dapphub"
+  },
+  {
+    "name": "E.G. Galano",
+    "link": "https://twitter.com/egalano",
+    "picture": "images/speakers/4.jpg",
+    "company": "Infura"
+  },
+]
