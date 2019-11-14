6f147df7efa8e8e5d4e11e74da17bceb4b4de915
Bettina Boon Falleur <bettina.boonfalleur@gmail.com>
2019-11-14 01:13:22 +0100

    speakers js

--- js/speaker.js
+++ js/speaker.js
@@ -1,49 +1,22 @@
 console.log(speakers)
 
-var speakersRowFirst = $('#speakersRowFirst');
-var speakersRowRest = $('#speakersRowRest');
-var speakerTemplateFirst = $('#speakerTemplateFirst');
-var speakerTemplateRest = $('#speakerTemplateRest');
+var speakersRow = $('#speakersRow');
+var speakerTemplate = $('#speakerTemplate');
 
-for (i = 0; i < 8; i ++) {
+
+for (i = 0; i < speakers.length; i ++) {
   console.log(speakers[i])
   if(speakers[i].link != undefined && speakers[i].link != '') {
-    speakerTemplateFirst.find('.firstName').wrap('<a href="'+speakers[i].link+'"></a>');
+    speakerTemplate.find('.firstName').wrap('<a href="'+speakers[i].link+'"></a>');
   }
 
-  speakerTemplateFirst.find('.firstName').text(speakers[i].firstName);
-  speakerTemplateFirst.find('.lastName').text(speakers[i].lastName);
-  speakerTemplateFirst.find('img').attr('src', speakers[i].picture);
-  speakerTemplateFirst.find('.company').text(speakers[i].company);
-  speakersRowFirst.append(speakerTemplateFirst.html());
+  speakerTemplate.find('.firstName').text(speakers[i].firstName);
+  speakerTemplate.find('.lastName').text(speakers[i].lastName);
+  speakerTemplate.find('img').attr('src', speakers[i].picture);
+  speakerTemplate.find('.company').text(speakers[i].company);
+  speakersRow.append(speakerTemplate.html());
 
-  if (speakerTemplateFirst.find('.firstName').parent().is( "a" )) {
-  speakerTemplateFirst.find('.firstName').unwrap();
+  if (speakerTemplate.find('.firstName').parent().is( "a" )) {
+  speakerTemplate.find('.firstName').unwrap();
   }
 }
-
-var showMore = $('#showMore');
-
-showMore
-  .one("click", function () {
-    for (var i = 12; i < speakers.length; i ++) {
-  if(speakers[i].link != undefined && speakers[i].link != '') {
-    speakerTemplateRest.find('.firstName').wrap('<a href="'+speakers[i].link+'"></a>');
-  }
-
-  speakerTemplateRest.find('.firstName').text(speakers[i].firstName);
-  speakerTemplateFirst.find('.lastName').text(speakers[i].lastName);
-  speakerTemplateRest.find('img').attr('src', speakers[i].picture);
-  speakerTemplateRest.find('.company').text(speakers[i].company);
-  speakersRowRest.append(speakerTemplateRest.html());
-
-  if (speakerTemplateRest.find('.firstName').parent().is( "a" )) {
-    speakerTemplateRest.find('.firstName').unwrap();
-  } }
-  })
-  .click(function() {
-    speakersRowRest.toggle();
-  })
-  .on('click', function(){
-    $(this).html() == "Show less" ? $(this).html('Show more') : $(this).html('Show less');
-  });
